d242c46b2f35e3aba2c6d4641972edd0ae06d32a
Ian <50678971+DreamHollow@users.noreply.github.com>
2021-10-30 14:14:29 -0400

    Added commentary

--- Project PageMagic/Project PageMagic-VS/pch.h
+++ Project PageMagic/Project PageMagic-VS/pch.h
@@ -1,5 +1,6 @@
 #ifndef PCH_H
 #define PCH_H
+// Not a traditional PCH to my understanding.
 // I was once told that "PRE-COMPILED HEADERS ARE THE DEVIL!" but I don't believe them.
 // Moved some inclusions over to Page.h due to slight overload
 
